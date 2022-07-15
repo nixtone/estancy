ddc201d3cf6d0f73a0ce77184a1a886fd8accb73
nixtone <nixtone@nxt.ru>
2022-07-16 00:37:28 +0300

    Дополнение структуры, графики и шрифт, табы в шапке

--- static/custom.js
+++ static/custom.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 	
+	// Табы в шапке
+	let tabAreaName = 'tabCount_';
+	$(".tab-area").each(function(index, el) {
+		let savedTabCount = localStorage.getItem(tabAreaName+$(this).data('name'));
+		if(savedTabCount != null) {
+			$(".tab-area .tab-item").removeClass('active');
+			$(".tab-area .tab-item.c"+savedTabCount).addClass('active');
+		}
+		else {
+			$(".tab-area .tab-item.c1").addClass('active');
+		}
+	});
+	$(".tab-label .tab-item").click(function(event) {
+		let tabCount = $(this).index()+1;
+		localStorage.setItem(tabAreaName+$(this).closest('.tab-area').data('name'), tabCount);
+		$(this).addClass('active').siblings().removeClass('active');
+		$(this).closest('.tab-area').find('.tab-page .tab-item.c'+tabCount).addClass('active').siblings().removeClass('active');
+	});
+
 });
